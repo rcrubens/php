eb47408fbd3382b0c9ec08365b718e3551a86b87
Rubens <63239288+rcrubens@users.noreply.github.com>
2023-02-08 14:25:21 -0300

    Teste de funcionalidade básica do PHP

---
+++ repeticao.php
@@ -0,0 +1,9 @@
+<?php
+    $produtos[0] = 'feijão';
+    $produtos[1] = 'arroz';
+    $produtos[2] = 'frango';
+
+    for ($indice=0; $indice < 3; $indice++) { 
+        echo "Produto índice $indice: $produtos[$indice].\n";
+    }
+?>  
