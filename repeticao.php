a4f602f53b10996985bbc56a4a1a7b37b1ef2bed
Rubens Campos <rcrubens@live.com>
2023-05-17 21:54:23 -0300

    Reorganização de pasta

--- repeticao.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $produtos[0] = 'feijão';
-    $produtos[1] = 'arroz';
-    $produtos[2] = 'frango';
-
-    for ($indice=0; $indice < 3; $indice++) { 
-        echo "Produto índice $indice: $produtos[$indice].\n";
-    }
-?>  
