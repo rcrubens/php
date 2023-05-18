f8854be1212a9e213ae4c393f9e5120a20d60c58
Rubens <63239288+rcrubens@users.noreply.github.com>
2023-05-17 23:52:24 -0300

    adicionando comentário

--- desafios/tabuada.php
+++ desafios/tabuada.php
@@ -5,6 +5,6 @@ $num = 5;
 echo "Tabuada do número \"$num\":" . PHP_EOL;
 
 for ($i=0; $i <= 10; $i++) { 
-    $produto = $i * $num;
+    $produto = $i * $num; // poderia tbm concatenar a operação na impressão abaixo
     echo "$i X $num = $produto" . PHP_EOL;
 }
